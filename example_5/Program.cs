02e9e30e70783d4c827876bc8b07806244aff9cb
Best Cat <kotova-irina11@rambler.ru>
2022-09-01 18:45:15 +0300

    решение задачи создание массива из случайных чисел

---
+++ example_5/Program.cs
@@ -0,0 +1,32 @@
+﻿
+//void не возвращает значение, не нужно исп return
+// создание массива из слуайных чисел
+void FillArray(int[] collection)
+{
+ int length = collection.Length;
+ int index = 0;
+ while (index < length)
+
+ {
+    collection[index] = new Random().Next(1, 10);
+    index++;
+ }
+}
+
+
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+{
+    Console.WriteLine(col[position]);
+    position++;
+}
+}
+
+int [] array = new int [10]; // new int[число]; создать массив с определённым кол-ом чисел.изначально будет заполнен нулями
+
+FillArray(array);
+PrintArray(array);
