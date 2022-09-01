92501e29ad4ac07761f9262af735981068b65259
Best Cat <kotova-irina11@rambler.ru>
2022-09-01 18:25:12 +0300

    решение задачи c массивом, нахождение индексв искомого числа

---
+++ example_4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Есть массив array, который состоит из элементов n
+//Найти элемент массива равный find
+// Нужно: 1. установить счётчик index в позицию 0
+//2. Если array[index]=find----завершение алгоритма
+//3. index= index+1
+//4. Если index < n, то перейти к шагу 2 или алгоритм завершим работу
+
+int [] array = {1,12,31,4,15,16,17,18};
+int n = array.Length;
+int find = 18;
+int index = 0;
+ while (index < n)
+ {
+    if (array [index] == find)
+    {
+    Console.WriteLine(index);
+    break; // если несколько одинаковых числа в массиве
+    }
+    index = index + 1;
+
+ }
+
+
