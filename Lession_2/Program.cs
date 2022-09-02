dcd86d00f1424e8c97cdb44de767b1c46e80db9c
Best Cat <kotova-irina11@rambler.ru>
2022-09-02 20:27:59 +0300

    семинар 02.09.22 задача 1

--- Lession_2/Program.cs
+++ Lession_2/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿// случайное число из отрезка 10-99 и показывает max цифру числа
+Console.Clear();
+int num = new Random().Next(10,100);
+int a1 = num/10;
+int a2 = num %10;
+int max = a1;
+if (max < a2)
+{
+    max = a2;
+}
+Console.WriteLine($"Максимльная цифра в числе {num}  --> {max}");
